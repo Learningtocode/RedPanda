557c16253b6914ffed2c0d1959ed0ae9991663a4
Kiera <JustForLaughts66@gmail.com>
2015-05-20 09:20:08 -0700

    I am still trying to figure out the navigation. So far, I have nothing. Tonight is going to be a long night of coding.

--- index.php
+++ index.php
@@ -6,12 +6,29 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <title>Muse</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css"> 
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css"> 
+        <link type="text/css" rel="stylesheet" href="css/homestyle.css">   
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>   
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+        <button id="expand-navigation">-</button>
+
+<section class="wrapper opened">
+  <ul>
+    <li><a href="#"><span class="icon-tumblr"></span></a></li>
+    <li><a href="#"><span class="icon-facebook"></span></a></li>
+    <li><a href="#"><span class="icon-google-plus"></span></a></li>
+    <li><a href="#"><span class="icon-dribbble"></span></a></li>
+    <li><a href="#"><span class="icon-github"></span></a></li>
+    <li><a href="#"><span class="icon-twitter"></span></a></li>
+  </ul>
+</section>
+
+<div class="overlay on-overlay"></div>
 </html>
