472afb437bf5b0cb09e40eebd16c969375823fb9
Kiera <JustForLaughts66@gmail.com>
2015-05-28 00:04:57 -0700

    I did the styling for box format, but much help is needed to fix it.

---
+++ customs.js
@@ -0,0 +1,6 @@
+$('#trackA').append('<embed id="embed_playerA" src="audio.mp3" autostart="true" hidden="true"></embed>');
+$(document).ready( function() {
+$('#Adele').click(function(){
+   $('#trackA').append('<embed id="embed_playerA" src="audio.mp3" autostart="true" hidden="true"></embed>');
+});
+});
--- index.php
+++ index.php
@@ -12,7 +12,9 @@ and open the template in the editor.
         <link type="text/css" rel="stylesheet" href="css/bootstrap.css"> 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css"/>
-        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>   
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>  
+        <script type="text/javascript" src="jquery-1.7.1.js"></script>
+        <script type="text/javascript">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>        
         <link type="text/css" rel="stylesheet" href="css/styling.css">   
@@ -23,13 +25,58 @@ and open the template in the editor.
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">New User</a></li>
         <li><a href="<?php echo $path . "controller/logout-user.php"; ?>">Logout</a></li>               
-        <li><a href="">Search</a></li>
     </ul>
     </header>
-    <body onload="setupBlocks();">
-       <div class="block">
-           <img src="images/Adele Profile.jpg"> 
-           <figcaption>Adele</figcaption>
+    <body>
+       <div id="columns">
+           
+        <figure>
+            <img name="Adele" src="images/Adele Profile.jpg" width="180" height="180" border="0" id="Adele" alt=""  />
+            <div id="trackA"></div>
+	<figcaption></figcaption>
+	</figure>
+           </div>
+	
+	<figure>
+            <img >
+            <div id="Kendrick"></div>
+	<figcaption></figcaption>
+	</figure>
+         
+        <figure>
+            <div id="Lana"><img src=""></div>
+	<figcaption></figcaption>
+	</figure>
+  
+	<figure>
+	<img src="">
+	<figcaption></figcaption>
+	</figure>
+	
+   <figure>
+	<img src="">
+	<figcaption></figcaption>
+	</figure>
+	
+   <figure>
+	 <img src="">
+	<figcaption></figcaption>
+	</figure>
+  
+	<figure>
+	<img src="">
+	<figcaption></figcaption>
+	</figure>	
+  
+   <figure>
+	<img src="">
+	<figcaption>Ariel wearing an evening gown of the 1890’s</figcaption>
+	</figure>
+  
+    <figure>
+	<img src="">
+    <figcaption></figcaption>
+	</figure>	
        </div>
     </body>
 </html>
