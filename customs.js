2283e62554ca172d3faa75f3ce64e1846c6ef607
Kiera <JustForLaughts66@gmail.com>
2015-05-28 03:55:41 -0700

    I organized the format. I have to finish adding music. The navigation is beginning difficult.

--- customs.js
+++ customs.js
@@ -1,27 +1,6 @@
-$('#trackA').append('<embed id="embed_playerA" src="music/Pavement.mp3" autostart="true" hidden="true"></embed>');
-$(document).ready( function() {
-$('#Adele').click(function(){
-   $('#trackA').append('<embed id="embed_playerA" src="music/Pavement.mp3" autostart="true" hidden="true"></embed>');
-});
-});
-
-$('#trackK').append('<embed id="embed_playerK" src="music/Money Trees.mp3" autostart="true" hidden="true"></embed>');
-$(document).ready( function() {
-$('#Kendrick').click(function(){
-   $('#trackK').append('<embed id="embed_playerK" src="music/Money Trees.mp3" autostart="true" hidden="true"></embed>');
-});
-});
-
-$('#trackL').append('<embed id="embed_playerL" src="music/Blue Jeans.mp3" autostart="true" hidden="true"></embed>');
-$(document).ready( function() {
-$('#Lana').click(function(){
-   $('#trackL').append('<embed id="embed_playerL" src="music/Blue Jeans.mp3" autostart="true" hidden="true"></embed>');
-});
-});
-
-$('#trackJC').append('<embed id="embed_playerJC" src="music/She Knows.mp3" autostart="true" hidden="true"></embed>');
-$(document).ready( function() {
-$('#Cole').click(function(){
-   $('#trackJC').append('<embed id="embed_playerJC" src="music/She Knows.mp3" autostart="true" hidden="true"></embed>');
-});
+$(".nav-btn").click(function(){
+      $("#menu").toggleClass("active");
+      $("#content").toggleClass("active");
+      $(".nav-btn").toggleClass("active");
+      $("h1").toggleClass("active");
 });
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>PlainSong</title>
+        <title>PlainSong: Artists</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css"> 
         <link type="text/css" rel="stylesheet" href="css/bootstrap.css"> 
@@ -19,51 +19,107 @@ and open the template in the editor.
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>        
         <link type="text/css" rel="stylesheet" href="css/styling.css">   
     </head>
-    <header>
-    <ul class="nav">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">New User</a></li>
-        <li><a href="<?php echo $path . "controller/logout-user.php"; ?>">Logout</a></li>               
-    </ul>
-    </header>
     <body>
+        <h1 class="plaintext">PlainSong</h1>
+        <div class="stuck-head">
+            <a class="nav-btn" id="nav-open" href="#sidebar"menu><span>menu</span></a>
+        </div>
+            <!--off canvas nav-->
+            <div id="menu">
+                <nav class="navigation">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">New User</a></li>
+                        <li><a href="<?php echo $path . "controller/logout-user.php"; ?>">Logout</a></li>
+                    </ul>
+                </nav>
+            </div>
+ 
        <div id="columns">
            
-        <figure>
-            <img name="Adele" src="images/Adele Profile.jpg"  id="Adele" alt=""  />
-            <div id="trackA"></div>
-	<figcaption>Adele - Chasing Pavement</figcaption>
-	</figure>
+           <figure>
+               <img name="Adele" src="images/Adele Profile.jpg"  id="Adele" alt=""  />
+               <figcaption>
+                   <p>Adele - Chasing Pavement</p>
+                   <audio controls>
+                   <source src="music/Pavements.mp3" >
+               </figcaption>
+           </figure>
+
+           <figure>
+               <img name="Kendrick" src="images/Kendrick Profile.jpg"  id="Kendrick" alt=""  />
+               <figcaption>
+                   <p>Kendrick Lamar - Money Trees</p>
+                    <audio controls>
+                     <source src="music/.mp3" >
+               </figcaption>
+           </figure>
+
+           <figure>
+               <img name="Chance" src="images/Chance Profile.jpg"  id="Chance" alt=""  />
+               <figcaption>
+                   <p>Chance The Rapper - Prom Night</p>
+                    <audio controls>
+                     <source src="music/.mp3" >
+               </figcaption>
+           </figure>
            
-	<figure>
-            <img name="Kendrick" src="images/Kendrick Profile.jpg"  id="Kendrick" alt=""  />
-            <div id="trackK"></div>
-	<figcaption>Kendrick Lamar - Money Trees</figcaption>
-	</figure>
-         
-        <figure>
-            <img name="Lana" src="images/Lana Profile.jpg"  id="Lana" alt=""  />
-            <div id="trackL"></div>
-	<figcaption>Lana Del Rey - Blue Jeans</figcaption>
-	</figure>
-  
-	<figure>
-            <img name="Cole" src="images/Cole Profile.jpg" id="Cole" alt=""  />
-            <div id="trackJC"></div>
-	<figcaption>J Cole - She Knows</figcaption>
-	</figure>
-	
-   <figure>
-	<img name="" src="images/" id="" alt=""  />
-	<figcaption></figcaption>
-	</figure>
-	
-   <figure>
-	 <img name="" src="images/" id="" alt=""  />
-	<figcaption></figcaption>
-	</figure>
-  	
+           <figure>
+               <img name="Lana" src="images/Lana Profile.jpg"  id="Lana" alt=""  />
+               <figcaption>
+                   <p>Lana Del Rey - Blue Jeans</p>
+                   <audio controls>
+                   <source src="music/.mp3" >
+               </figcaption>
+           </figure>
+
+           <figure>
+               <img name="Cole" src="images/Cole Profile.jpg" id="Cole" alt=""  />
+               <div id="trackJC"></div>
+               <figcaption>
+                   <p>J Cole - She Knows</p>
+                   <audio controls>
+                   <source src="music/.mp3" >
+               </figcaption>
+           </figure>
+
+           <figure>
+               <img name="21" src="images/21 Pilots Profile.jpg" id="21" alt=""  />
+               <figcaption>
+                   <p>21 Pilots - Tear In My Heart</p>
+                   <audio controls>
+                   <source src="music/.mp3" >
+               </figcaption>
+           </figure> 
+           
+           <figure>
+               <img name="Dumb" src="images/Dumbfounded Profile.jpg" id="Dumb" alt=""  />
+               <figcaption>
+                   <p>Dumbfoundead - 24KTOWN</p>
+                   <audio controls>
+                   <source src="music/.mp3" >
+               </figcaption>
+           </figure>
+
+           <figure>
+               <img name="Aiko" src="images/Jhene Aiko Profile.jpg" id="Aiko" alt=""  />
+               <figcaption>
+                   <p>Jhene Aiko - Spotless Mind</p>
+                   <audio controls>
+                   <source src="music/.mp3" >
+               </figcaption>
+           </figure>
+           
+           <figure>
+               <img name="Amy" src="images/Winehouse Profile.jpg" id="Amy" alt=""  />
+               <figcaption>
+                   <p>Amy Winehouse - Tears Dry On Their Own</p>
+                   <audio controls>
+                   <source src="music/.mp3" >
+               </figcaption>
+           </figure>
+           
        </div>
     </body>
 </html>
